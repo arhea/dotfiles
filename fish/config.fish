b21f094cbd0c6350e19c67749062f52633ef38ad
Alex Rhea <alex.rhea@gmail.com>
2016-05-05 09:57:19 -0400

    updating the path

--- fish/config.fish
+++ fish/config.fish
@@ -6,8 +6,12 @@ set default_path /usr/bin /usr/sbin /bin /sbin
 set rvm_path $HOME/.rvm/bin
 set homebrew_path /usr/local/bin /usr/local/sbin (brew --prefix coreutils)/libexec/gnubin (brew --prefix homebrew/php/php56)/bin
 set composer_path ~/.composer/vendor/bin
+set google_cloud_path ~/code/google-cloud-sdk/bin
 
-set -g -x PATH $homebrew_path $rvm_path $composer_path $default_path
+set -g -x PATH $homebrew_path $rvm_path $composer_path $google_cloud_path $default_path
+
+# setup go
+set -x GOPATH ~/code/go
 
 # setup nvm
 test -s ~/.nvm-fish/nvm.fish; and source ~/.nvm-fish/nvm.fish
