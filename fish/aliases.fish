7d9ab910a15d8b90d9757b2173fe5c34a5644219
Alex Rhea <alex.rhea@gmail.com>
2017-04-23 09:16:25 -0400

    adding more aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,5 +1,16 @@
+# Adapted from https://github.com/jessfraz/dotfiles
+
+# Easier navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
 # Pipe my public key to my clipboard.
 alias pubkey="cat ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 
 # Pipe my private key to my clipboard.
 alias prikey="cat ~/.ssh/id_rsa | pbcopy | echo '=> Private key copied to pasteboard.'"
+
+# vhosts
+alias hosts='sudo $EDITOR /etc/hosts'
